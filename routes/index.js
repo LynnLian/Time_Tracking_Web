87e65ebf74928aedb81b954db2b9b25e715848a3
LynnLian <zw.lian2010@gmail.com>
2018-08-15 15:25:13 +0200

    add:  - database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const models = require('../models');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Express'});
+});
+
+router.post('/start', function (req, res) {
+
+  const startTime = req.body.startTime;
+  console.log(startTime);
+
+  models.records.create({
+    startTime: startTime
+  })
+
 });
 
 module.exports = router;
